f0f205928e63d71b3194a0613558f2d10614ebe2
Daniel  Morton <dacmorton@gmail.com>
2024-03-25 09:58:13 -0400

    format

--- src/write.rs
+++ src/write.rs
@@ -5,11 +5,11 @@ use std::fs::File;
 pub(super) fn write_csv(df: &mut DataFrame, file_name: &str) {
     let file = match File::create(file_name) {
         Ok(f) => f,
-        Err(e) => panic!("{}", e),
+        Err(e) => panic!("{:?}", e)
     };
     match CsvWriter::new(&file).has_header(true).finish(df) {
         Ok(_) => (),
-        Err(e) => panic!("{:?}", e),
+        Err(e) => panic!("{:?}", e)
     }
 }
 
@@ -17,18 +17,12 @@ pub(super) fn write_by_year(df: &DataFrame, file_name: &str, years: &HashSet<i32
     years.iter().for_each(|&y| {
         let file_split = file_name.split('/').map(String::from).collect::<Vec<_>>();
         let year_file = format!("{}/{}_{}", file_split[0], y, file_split[1]);
-        let col = match df.column("Year") {
-            Ok(c) => c,
-            Err(e) => panic!("{:?}", e),
-        };
-        let mask = match col.equal(y) {
-            Ok(m) => m,
-            Err(e) => panic!("{:?}", e),
-        };
-        let mut fdf = match df.filter(&mask) {
+        let mut write_df = match df.column("Year")
+            .and_then(|s| s.equal(y))
+            .and_then(|mask| df.filter(&mask)) {
             Ok(fdf) => fdf,
-            Err(e) => panic!("{:?}", e),
+            Err(e) => panic!("{:?}", e)
         };
-        write_csv(&mut fdf, &year_file)
+        write_csv(&mut write_df, &year_file)
     })
 }
